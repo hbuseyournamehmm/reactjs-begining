021da2a5ad3d2f1c5cc7307404ee99626c56ed6c
hbuseyournamehmm <unishabaram@gmail.com>
2021-03-18 15:32:36 +0545

    third commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './First';
+import Name from './Name';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>hello world</h1>
+      <First/>
+      <Name/> */}
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/First.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const First=()=> {
+    return (
+        <>
+          <p>this is a react js, This is our first component build from the react js</p>  
+        </>
+    )
+}
+
+export default First
